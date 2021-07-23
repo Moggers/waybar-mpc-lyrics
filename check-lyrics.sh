25b35d7c747e4ea15ad10ed012c3c0852e596ea2
Matt <mattkramara@gmail.com>
2021-07-24 00:30:45 +1000

    Handle bad track titles gracefully

--- check-lyrics.sh
+++ check-lyrics.sh
@@ -24,32 +24,38 @@ urldecode() {
     printf '%b' "${url_encoded//%/\\x}"
 }
 
+BAD_TRACK=""
 while [ 1 ]
 do
 	SONG_SLUG="$(mpc current -f '%artist% %title%')"
 	ENCODED="$(urlencode "$SONG_SLUG")"
-	if [ ! -z "$ENCODED" ]
-	then
+	if [[ ! -z "$ENCODED" ]] && [[ "$ENCODED" != "$BAD_TRACK" ]]; then
 		REGEX='([0-9]+):([0-9]+)'
 		RES="$(curl "https://api.textyl.co/api/lyrics?q=$ENCODED")"
-		COUNTER=1
-		LAST_LINE="\"None\""
+		if [ "$RES" == "No lyrics available" ]; then
+			BAD_TRACK="$ENCODED"
+			continue
+		fi
+		COUNTER=0
+		LAST_LINE="\"...\""
 		while [ "$LAST_LINE" != "null" ]
 		do
+			SONG_SLUG="$(mpc current -f '%artist% %title%')"
+			NEW_ENCODED="$(urlencode "$SONG_SLUG")"
+			if [ $NEW_ENCODED != $ENCODED ]; then
+				break
+			fi
 			POSITION=$(mpc status | grep playing | sed 's/^.* \(.\+:.\+\)\/.*$/\1/')
-			if [[ $POSITION =~ $REGEX ]]
-			then
+			if [[ $POSITION =~ $REGEX ]]; then
 				SECONDS=$((10#${BASH_REMATCH[1]} * 60 + 10#${BASH_REMATCH[2]}))
 			else
 				break
 			fi
 			LYRIC_SECONDS="$(echo "$RES" | jq ".[$COUNTER].seconds")"
-			if [ "$LYRIC_SECONDS" != "null" ]
-			then
-				if [ "$SECONDS" -gt "$LYRIC_SECONDS" ] 
-				then
+			if [ "$LYRIC_SECONDS" != "null" ]; then
+				if [ "$SECONDS" -gt "$LYRIC_SECONDS" ]; then
+					LAST_LINE=$(echo "$RES" | jq ".[$(($COUNTER))].lyrics")
 					COUNTER=$(($COUNTER+1))
-					LAST_LINE=$(echo "$RES" | jq ".[$(($COUNTER-1))].lyrics")
 				fi
 			fi
 			echo "{\"text\": $LAST_LINE}"
